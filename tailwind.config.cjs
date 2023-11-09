3c4980a4d704cec882dbc064da56d96899022e95
Nikhilesh <nikhileshmeher24@gmail.com>
2023-11-09 22:56:12 +0530

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#040D12",
+        secondary: "#5C8374",
+        tertiary: "#183D3D",
+        "black-100": "#5C8374",
+        "black-200": "#000000",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
